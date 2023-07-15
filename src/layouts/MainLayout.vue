dd658b8721a322236c6387da1ce555b1985c87ca
lambiv-dzenyuy <gillslambiv@gmail.com>
2023-07-15 02:48:24 +0100

    feat: add svg for landing page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,9 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header bordered class="bg-white header text-primary">
       <q-toolbar>
         <q-btn
+          v-if="$q.screen.lt.sm"
           flat
           dense
           round
@@ -11,13 +12,32 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title class="q-ml-xl">
+          <q-avatar outlined size="34px" color="white">LD</q-avatar>
+        </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div class="headings">
+          <q-tabs
+            no-caps
+            active-color="primary"
+            indicator-color="transparent"
+            class="text-grey-8"
+            v-model="tab"
+          >
+            <q-tab name="images" label="Home" />
+            <q-tab name="videos" label="About Me" />
+            <q-tab name="articles" label="Skills" />
+            <q-tab name="articles" label="Projects" />
+            <q-tab name="articles" label="Contact Me" />
+          </q-tabs>
+        </div>
+        <q-btn flat class="btn-hire-me justify-start text-white text-no-wrap"
+          >Hire me</q-btn
+        >
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-drawer v-model="leftDrawerOpen" bordered>
       <q-list>
         <q-item-label header> Essential Links </q-item-label>
 
@@ -29,8 +49,10 @@
       </q-list>
     </q-drawer>
 
-    <q-page-container>
-      <router-view />
+    <q-page-container class="page-container overflow-hidden">
+      <q-page class="column o">
+        <router-view />
+      </q-page>
     </q-page-container>
   </q-layout>
 </template>
@@ -92,3 +114,26 @@ function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
+<Style lang="scss" scoped>
+.header {
+  padding: 0px 200px;
+}
+.page-container {
+  overflow: hidden;
+}
+.headings {
+  width: 70vw;
+}
+
+.btn-hire-me {
+  border-radius: 10px;
+  border-radius: 10px;
+  background: linear-gradient(
+    199deg,
+    #0b1f46 0%,
+    #00638f 32.29%,
+    #00afbe 64.06%,
+    #6efacd 100%
+  );
+}
+</Style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,30 @@
 <template>
-  <q-page class="row items-center justify-evenly">
+  <div>
+    <div class="container bg-primary">
+      <div class="test full-width"></div>
+    </div>
     <example-component
       title="Example component"
       active
       :todos="todos"
       :meta="meta"
-    ></example-component>
-  </q-page>
+    >
+    </example-component>
+    <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    >
+    </example-component>
+    <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    >
+    </example-component>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,26 +35,53 @@ import { ref } from 'vue';
 const todos = ref<Todo[]>([
   {
     id: 1,
-    content: 'ct1'
+    content: 'ct1',
   },
   {
     id: 2,
-    content: 'ct2'
+    content: 'ct2',
   },
   {
     id: 3,
-    content: 'ct3'
+    content: 'ct3',
   },
   {
     id: 4,
-    content: 'ct4'
+    content: 'ct4',
   },
   {
     id: 5,
-    content: 'ct5'
-  }
+    content: 'ct5',
+  },
 ]);
 const meta = ref<Meta>({
-  totalCount: 1200
+  totalCount: 1200,
 });
 </script>
+
+<style>
+.container {
+  background: black;
+  z-index: -1;
+}
+.test {
+  z-index: 1;
+  height: 50vh;
+  background: url(../assets/f.svg);
+
+  filter: blur(200px);
+
+  animation: left-to-right 20s ease-in forwards infinite;
+  transition: gradient rotate(380deg);
+}
+
+.test:hover {
+  animation-play-state: paused;
+}
+
+@keyframes left-to-right {
+  0% {
+    transform: rotate(360deg);
+  }
+}
+</style>
